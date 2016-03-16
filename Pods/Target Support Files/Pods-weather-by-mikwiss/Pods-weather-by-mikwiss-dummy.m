e68fb3022a06aff5291a528222d77a71969f13ce
mbret <mathieu.bret@gmail.com>
2016-03-16 13:14:38 +0100

    Add Alamofire

---
+++ Pods/Target Support Files/Pods-weather-by-mikwiss/Pods-weather-by-mikwiss-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weather_by_mikwiss : NSObject
+@end
+@implementation PodsDummy_Pods_weather_by_mikwiss
+@end
--- weather-by-mikwiss/WeatherService.swift
+++ weather-by-mikwiss/WeatherService.swift
@@ -14,6 +14,9 @@ class WeatherService {
     /// MARK : Properties
     ///
     private var apiKey : String = "8919a910467221c964821b0dd40f7916";
+    private var urlBAse : String = "api.openweathermap.org/";
+    private var urlData25 : String = "data/2.5/"
+    private var urlWeatherForCityID : String = "weather?id="
     
     ///
     /// MARK : Init
